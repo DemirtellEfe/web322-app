839617d1db2f99c3092733de0aaae7500a1cf5b6
DemirtellEfe <76067684+DemirtellEfe@users.noreply.github.com>
2025-04-13 22:50:21 -0400

    Add files via upload

---
+++ auth-service.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+let User;
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String, unique: true },
+  password: String,
+  email: String,
+  loginHistory: [{
+    dateTime: Date,
+    userAgent: String
+  }]
+});
+
+module.exports.initialize = function () {
+  return new Promise((resolve, reject) => {
+    const db = mongoose.createConnection(process.env.MONGODB_CONN_STRING);
+
+    db.on("error", (err) => {
+      reject(err);
+    });
+
+    db.once("open", () => {
+      User = db.model("users", userSchema);
+      resolve();
+    });
+  });
+};
+
+module.exports.registerUser = function (userData) {
+  return new Promise((resolve, reject) => {
+    if (userData.password !== userData.password2) {
+      reject("Passwords do not match");
+      return;
+    }
+
+    bcrypt.hash(userData.password, 10)
+      .then((hash) => {
+        userData.password = hash;
+
+        const newUser = new User({
+          userName: userData.userName,
+          password: userData.password,
+          email: userData.email,
+          loginHistory: []
+        });
+
+        newUser.save()
+          .then(() => resolve())
+          .catch((err) => {
+            if (err.code === 11000) {
+              reject("User Name already taken");
+            } else {
+              reject("There was an error creating the user: " + err);
+            }
+          });
+      })
+      .catch(() => {
+        reject("There was an error encrypting the password");
+      });
+  });
+};
+
+module.exports.checkUser = function (userData) {
+  return new Promise((resolve, reject) => {
+    User.find({ userName: userData.userName })
+      .then((users) => {
+        if (users.length === 0) {
+          reject(`Unable to find user: ${userData.userName}`);
+          return;
+        }
+
+        bcrypt.compare(userData.password, users[0].password)
+          .then((result) => {
+            if (!result) {
+              reject(`Incorrect Password for user: ${userData.userName}`);
+              return;
+            }
+
+            users[0].loginHistory.push({
+              dateTime: new Date(),
+              userAgent: userData.userAgent
+            });
+
+            User.updateOne(
+              { userName: users[0].userName },
+              { $set: { loginHistory: users[0].loginHistory } }
+            )
+              .then(() => resolve(users[0]))
+              .catch(err => reject(`There was an error verifying the user: ${err}`));
+          })
+          .catch(err => reject(`Error comparing passwords: ${err}`));
+      })
+      .catch(() => {
+        reject(`Unable to find user: ${userData.userName}`);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 05
+*  WEB322 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 *  No part of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 *
-*  Name: Efe Demirtel  Student ID: 126378223  Date: 2025-04-07
+*  Name: Efe Demirtel  Student ID: 126378223  Date: 2025-04-13
 *
 *  Replit Web App URL: https://replit.com/@edemirtel/web322-app?v=1
 *  GitHub Repository URL: https://github.com/DemirtellEfe/web322-app.git
@@ -19,6 +19,9 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 const storeService = require("./data/store-service.js");
 const expressLayouts = require("express-ejs-layouts");
+const authData = require("./auth-service");
+const clientSessions = require("client-sessions");
+
 
 const upload = multer();
 const PORT = process.env.PORT || 3000;
@@ -27,6 +30,18 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
+app.use(clientSessions({
+  cookieName: "session",
+  secret: "web322appSecretKey",
+  duration: 2 * 60 * 60 * 1000,
+  activeDuration: 1000 * 60 * 5 
+}));
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
 app.set("layout", "layouts/main");
 
 cloudinary.config({
@@ -39,13 +54,13 @@ cloudinary.config({
 // ROUTES
 app.get("/", (req, res) => res.render("about"));
 
-app.get("/items/add", (req, res) => {
+app.get("/items/add", ensureLogin, (req, res) => {
   storeService.getCategories()
     .then((cats) => res.render("addPost", { categories: cats }))
     .catch(() => res.render("addPost", { categories: [] }));
 });
 
-app.post("/items/add", upload.single("featureImage"), (req, res) => {
+app.post("/items/add", ensureLogin, upload.single("featureImage"), (req, res) => {
   const processItem = (imageUrl) => {
     req.body.featureImage = imageUrl;
     req.body.published = req.body.published ? true : false;
@@ -72,11 +87,12 @@ app.post("/items/add", upload.single("featureImage"), (req, res) => {
 
     streamUpload(req)
       .then((uploaded) => processItem(uploaded.url))
-      .catch((err) => res.status(500).send("File upload failed: " + err));
+      .catch((err) => res.status(500).send("File upload failed: " + (err.message || JSON.stringify(err))));
   } else {
     processItem("");
   }
 });
+
 
 app.get("/items", (req, res) => {
   storeService.getAllItems()
@@ -94,7 +110,7 @@ app.get("/items", (req, res) => {
     });
 });
 
-app.get("/items/delete/:id", (req, res) => {
+app.get("/items/delete/:id", ensureLogin, (req, res) => {
   storeService.deletePostById(req.params.id)
     .then(() => res.redirect("/items"))
     .catch(() => res.status(500).send("Unable to Remove Item / Item not found"));
@@ -112,21 +128,23 @@ app.get("/categories", (req, res) => {
     .catch(() => res.render("categories", { categories: [] }));
 });
 
-app.get("/categories/add", (req, res) => {
+app.get("/categories/add", ensureLogin, (req, res) => {
   res.render("addCategory");
 });
 
-app.post("/categories/add", (req, res) => {
+app.post("/categories/add", ensureLogin, (req, res) => {
   storeService.addCategory(req.body)
     .then(() => res.redirect("/categories"))
     .catch((err) => res.status(500).send("Failed to add category: " + err));
 });
 
-app.get("/categories/delete/:id", (req, res) => {
+
+app.get("/categories/delete/:id", ensureLogin, (req, res) => {
   storeService.deleteCategoryById(req.params.id)
     .then(() => res.redirect("/categories"))
     .catch(() => res.status(500).send("Unable to Remove Category / Category not found"));
 });
+
 
 app.get("/shop", async (req, res) => {
   try {
@@ -173,6 +191,7 @@ app.get("/shop/:id", async (req, res) => {
 });
 
 storeService.initialize()
+  .then(authData.initialize)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
@@ -181,3 +200,68 @@ storeService.initialize()
   .catch((err) => {
     console.error("Unable to start server:", err);
   });
+
+
+  function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+  }
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  authData.registerUser(req.body)
+    .then(() => {
+      res.render("register", { successMessage: "User created" });
+    })
+    .catch(err => {
+      res.render("register", {
+        errorMessage: err,
+        userName: req.body.userName
+      });
+    });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  req.body.userAgent = req.get("User-Agent");
+
+  authData.checkUser(req.body)
+    .then(user => {
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory
+      };
+      res.redirect("/items");
+    })
+    .catch(err => {
+      res.render("login", {
+        errorMessage: err,
+        userName: req.body.userName
+      });
+    });
+});
+
+app.get("/logout", (req, res) => {
+  req.session.reset();
+  res.redirect("/");
+});
+
+app.get("/userHistory", ensureLogin, (req, res) => {
+  res.render("userHistory");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { message: "Page Not Found" });
+});
+
+  
